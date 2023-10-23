f29e1a18ca604532dcfb39dc12a330aa821f43c6
Lucasss12 <lucas.mene.pro@gmail.com>
2023-10-23 21:14:30 +0200

    fix for vercel

--- src/app/components/contact/contact.jsx
+++ src/app/components/contact/contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function contact() {
   return (
-    <div id="contact" class="flex flex-col items-center justify-center pt-12 mt-24 bg-white shadow-lg">
+    <div id="contact" className="flex flex-col items-center justify-center pt-12 mt-24 bg-white shadow-lg">
         <h1 className='text-6xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500 mb-5'>Contact</h1>
         <p className='text-xl text-zinc-600'>Discutons de votre projet ensemble 👉🏻 <a className='font-semibold' href="mailto:lucas.mene.pro@gmail.com">lucas.mene.pro@gmail.com</a></p>
         <div>
--- src/app/private/page.jsx
+++ src/app/private/page.jsx
@@ -6,7 +6,7 @@ export default function page() {
     <div className='flex justify-center mt-44'>
       <div className='font-semibold text-center'>
         <p className='mb-4 text-6xl'>Ce site est privé</p>
-        <Link className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500 text-3xl' href='/'>Retour à la page d'accuei</Link>
+        <Link className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500 text-3xl' href='/'>Retour à la page d&apos;accuei</Link>
       </div>
     </div>
   )
