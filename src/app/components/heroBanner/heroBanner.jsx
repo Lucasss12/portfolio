6562ed32d46765d9fdc54125283378b2ad35cf6b
Lucasss12 <lucas.mene.pro@gmail.com>
2024-01-17 15:53:44 +0100

    refacto navbar

--- src/app/components/heroBanner/heroBanner.jsx
+++ src/app/components/heroBanner/heroBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.scss'
 import Link from 'next/link'
-import avatar from '../../image/avatar.png'
+import avatar from '../../../../public/image/avatar.png'
 import Image from 'next/image'
 
 
--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="px-6 py-6 flex justify-between content-center bg-white shadow-sm text-lg font-semibold">
+    <nav className="sm:px-20 px-6 py-5 flex justify-between content-center bg-white shadow-sm font-semibold items-center">
       <Link href="/">
-        <h1 className="mr-4 text-2xl lg:text-3xl xl:text-4xl">&lt; Lucas /&gt;</h1>
+        <h1 className="text-xl lg:text-3xl">&lt; Lucas /&gt;</h1>
       </Link>
-      <ul className="flex items-center space-x-4 text-lg">
+      <ul className="flex items-center sm:space-x-1 text-lg sm:text-xl">
         <li>
           <Link href="#projets">
-            <h1 className="py-1 px-3 sm:hover:bg-slate-400 sm:hover:bg-opacity-20 rounded-lg">
+            <h1 className="py-1 px-2 sm:hover:bg-slate-400 sm:hover:bg-opacity-20 rounded-lg">
               Projets
             </h1>
           </Link>
         </li>
         <li>
           <Link href="#contact">
-            <h1 className="py-1 px-3 sm:hover:bg-slate-400 sm:hover:bg-opacity-20 rounded-lg">
+            <h1 className="py-1 px-2 sm:hover:bg-slate-400 sm:hover:bg-opacity-20 rounded-lg">
               Contact
             </h1>
           </Link>
